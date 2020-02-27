e4e1b3a18b187547f39439040ec1a0c0b2016864
Frances Emmanuel Callado <32131146+cld-emman05@users.noreply.github.com>
2020-02-27 15:36:46 +0800

    added intent

--- app/src/main/java/com/example/shoppingcart/MainActivity.java
+++ app/src/main/java/com/example/shoppingcart/MainActivity.java
@@ -2,16 +2,29 @@ package com.example.shoppingcart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     Button shop, exit;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        shop = (Button) findViewById(R.id.shopbtn);
+        exit = (Button) findViewById(R.id.exitbtn);
+
+        shop.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                    }
+                                }
+        );
     }
 }
